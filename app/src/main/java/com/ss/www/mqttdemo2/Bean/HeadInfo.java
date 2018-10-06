48ae0f1bafc3db88e00e063b5f51d0752c8e299b
yangxuesong <498976239@qq.com>
2018-10-07 00:19:59 +0800

    修改了选中标记

---
+++ app/src/main/java/com/ss/www/mqttdemo2/Bean/HeadInfo.java
@@ -0,0 +1,35 @@
+package com.ss.www.mqttdemo2.Bean;
+
+/**
+ * Created by 小松松 on 2018/10/6.
+ */
+
+public class HeadInfo {
+    private String name;
+    private boolean mark;
+    private String count;
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMark() {
+        return mark;
+    }
+
+    public void setMark(boolean mark) {
+        this.mark = mark;
+    }
+}
